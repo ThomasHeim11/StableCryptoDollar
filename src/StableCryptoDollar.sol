9fe60683ad18bdc7897061bbb14c18dc2231b4e8
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-25 17:56:26 +0200

    update contract✅

--- src/StableCryptoDollar.sol
+++ src/StableCryptoDollar.sol
@@ -28,7 +28,6 @@ pragma solidity 0.8.19;
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-
 /*
  * @title StableCryptoDollar
  * @author Thomas Heim
@@ -39,6 +38,23 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
  */
 
-contract StableCryptoDollar {
-    constructor() {}
+contract StableCryptoDollar is ERC20Burnable, Ownable {
+    error StableCryptoDollar__MustBeMoreThanZero();
+    error StableCryptoDollar__BurnAmountExeedsBalance();
+
+
+    constructor() ERC20("StableCryptoDollar", "SCD") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert StableCryptoDollar__MustBeMoreThanZero();
+        }
+        else if (_amount > balance) {
+            revert StableCryptoDollar__BurnAmountExeedsBalance();
+        }
+        else {
+            super.burn(_amount);
+        }
+    }
 }
