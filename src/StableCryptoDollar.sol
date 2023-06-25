a6bcafdbc353737650419db8142a1e411450d412
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-25 18:10:22 +0200

    update contract✅

--- src/StableCryptoDollar.sol
+++ src/StableCryptoDollar.sol
@@ -1,66 +1,56 @@
 // SPDX-License-Identifier: MIT
 
-// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
-
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
-
 pragma solidity 0.8.19;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-/*
- * @title StableCryptoDollar
- * @author Thomas Heim
- * Collateral: Exogenous
- * Minting (Stability Mechanism): Decentralized (Algorithmic)
- * Value (Relative Stability): Anchored (Pegged to USD)
- * Collateral Type: Crypto
- * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+/** 
+ * @title DecentralizedStableCoin
+ * @author Patrick Collins
+ * @notice Collateral: Exogenous
+ * @notice Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * @notice Value (Relative Stability): Anchored (Pegged to USD)
+ * @notice Collateral Type: Crypto
+ *
+ * @dev This is the contract meant to be owned by DSCEngine. It is an ERC20 token that can be minted and burned by the DSCEngine smart contract.
  */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
-contract StableCryptoDollar is ERC20Burnable, Ownable {
-    error StableCryptoDollar__MustBeMoreThanZero();
-    error StableCryptoDollar__BurnAmountExeedsBalance();
-    error StableCryptoDollar__NotZeroAddress();
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
-    constructor() ERC20("StableCryptoDollar", "SCD") {}
-
+    /**
+     * @notice Burns a specific amount of tokens owned by the contract owner.
+     * @param _amount The amount of tokens to be burned.
+     * @dev Throws an error if `_amount` is less than or equal to zero or if the owner's balance is insufficient.
+     */
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert StableCryptoDollar__MustBeMoreThanZero();
-        } if (balance < _amount) {
-            revert StableCryptoDollar__BurnAmountExeedsBalance();
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
 
-    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
-        if(_to == address(0)){
-            revert StableCryptoDollar__NotZeroAddress();
+    /**
+     * @notice Mints new tokens and assigns them to a specific address.
+     * @param _to The address to which the newly minted tokens will be assigned.
+     * @param _amount The amount of tokens to be minted.
+     * @return A boolean value indicating whether the minting was successful.
+     * @dev Throws an error if `_to` address is zero or if `_amount` is less than or equal to zero.
+     */
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
         }
         if (_amount <= 0) {
-            revert StableCryptoDollar__MustBeMoreThanZero();
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
         }
         _mint(_to, _amount);
         return true;
