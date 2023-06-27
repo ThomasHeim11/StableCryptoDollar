5a0d7bd07d82d75c52af293cc8740faed295c33b
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-27 06:44:05 +0200

    update contract✅

--- script/DeploySCD..s.sol
+++ script/DeploySCD..s.sol
@@ -9,6 +9,8 @@ import {SCDEngine} from "../src/SCDEngine.sol";
 contract DeploySCD is Script {
     function run() external returns (StableCryptoDollar, SCDEngine) {
         vm.startBroadcast();
+        StableCryptoDollar SCD = new StableCryptoDollar();
+
         vm.stopBroadcast();
         
     }
