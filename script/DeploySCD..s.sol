435430f8dd0ba1ac9a5cf609f02ab6de837be363
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-27 06:41:52 +0200

    update contract✅

--- script/DeploySCD..s.sol
+++ script/DeploySCD..s.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {StableCryptoDollar} from "../src/StableCryptoDollar.sol";
+import {SCDEngine} from "../src/SCDEngine.sol";
 
 contract DeploySCD is Script {
-    function run() external returns(StableCryptoDollar, SCDEngine ) {}
+    function run() external returns (StableCryptoDollar, SCDEngine) {
+        
+    }
 }
