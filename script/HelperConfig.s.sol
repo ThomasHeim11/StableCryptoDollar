5172cea5c863905a2136e529cafc1c39da038d2d
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-27 18:59:34 +0200

    update contract✅

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,5 +8,22 @@ contract HelperConfig is Script{
     struct NewtworkConfig {
         address wethUsdPriceFeed;
         address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
+    }
+
+    NewtworkConfig public activeNetworkConfig;
+
+    constructor() {}
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
     }
 }
