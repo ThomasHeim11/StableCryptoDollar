60dc0020cedd95ee33c1b823be0272c93763d4c4
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-28 11:19:54 +0200

    update contract✅

---
+++ test/unit/SCDEngineTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeploySCD} from "../../script/DeploySCD.s.sol";
+
+contract SCDEngineTest is Test {
+    function setUp() public {}
+}
