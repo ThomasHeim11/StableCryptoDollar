ab5749212ae4ad79073094ec7e22be9aa485e989
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-28 11:25:21 +0200

    update contract✅

--- test/unit/SCDEngineTest.t.sol
+++ test/unit/SCDEngineTest.t.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {DeploySCD} from "../../script/DeploySCD.s.sol";
+import {StableCryptoDollar} from "../../src/StableCryptoDollar.sol";
+import {SCDEngine} from "../../src/SCDEngine.sol";
 
 contract SCDEngineTest is Test {
-    function setUp() public {}
+    DeploySCD deployer;
+    StableCryptoDollar scd;
+    SCDEngine scde;
+
+    function setUp() public {
+        deployer = new DeploySCD();
+        (scd, scde) = deployer.run();
+    }
 }
