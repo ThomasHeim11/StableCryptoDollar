5466e591dd992ffc04694d1be56982448345a296
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-28 10:42:07 +0200

    update contract✅

--- script/DeploySCD..s.sol
+++ script/DeploySCD..s.sol
@@ -5,13 +5,23 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {StableCryptoDollar} from "../src/StableCryptoDollar.sol";
 import {SCDEngine} from "../src/SCDEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploySCD is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (StableCryptoDollar, SCDEngine) {
-        vm.startBroadcast();
-        StableCryptoDollar SCD = new StableCryptoDollar();
-        // SCDEngine engine = new SCDEngine;
-        vm.stopBroadcast();
-        
+        HelperConfig config = new HelperConfig();
+
+       (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc,
+       uint256 deployerKey) =
+        config.activeNetworkConfig();
+
+       tokenAddresses = [weth, wbtc];
+       priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+       vm.startBroadcast();
+       StableCryptoDollar scd = new StableCryptoDollar();
     }
 }
