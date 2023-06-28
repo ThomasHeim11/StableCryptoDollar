59dffbbd7600bfb284e4243d630b064db61dd26a
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-28 15:49:52 +0200

    update contract✅

--- src/SCDEngine.sol
+++ src/SCDEngine.sol
@@ -42,7 +42,7 @@ contract SCDEngine is ReentrancyGuard {
     uint256 private constant PRECISION = 1e18;
     uint256 private constant LIQUIDATION_THRESHOLD = 50;
     uint256 private constant LIQUIDATION_PRECISION = 100;
-    uint256 private constant MIN_HELATH_FACTOR = 1;
+    uint256 private constant MIN_HELATH_FACTOR = 1e18;
 
     mapping(address token => address s_priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256)) private s_collateralDeposited;
@@ -158,7 +158,16 @@ contract SCDEngine is ReentrancyGuard {
         _revertIfHealthFactorIsBroken(msg.sender);
     }
 
-    function liquidate() external {}
+    function liquidate(address collateral, address user, uint256 debtToCover) 
+        external 
+        moreThanZero(debtToCover)
+        nonReentrant
+         {
+            uint256 startingUserHealthFactor = _healthFactor(user);
+            if(starting)
+
+        }
+    
 
     function getHelathFactor() external view {}
 
