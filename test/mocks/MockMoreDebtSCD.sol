1c313f3819f7d23cdb4956bf2258185f3ab900ac
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-30 09:21:10 +0200

    update contract✅

--- test/mocks/MockMoreDebtSCD.sol
+++ test/mocks/MockMoreDebtSCD.sol
@@ -10,14 +10,14 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MockV3Aggregator} from "./MockV3Aggregator.sol";
 
 /*
- * @title DecentralizedStableCoin
- * @author Patrick Collins
+ * @title StableCryptoDollar
+ * @author Thomas Heim
  * Collateral: Exogenous
  * Minting (Stability Mechanism): Decentralized (Algorithmic)
  * Value (Relative Stability): Anchored (Pegged to USD)
  * Collateral Type: Crypto
  *
- * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ * This is the contract meant to be owned by SCDEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
  */
 contract MockMoreDebtSCD is ERC20Burnable, Ownable {
     error StableCryptoDollar__AmountMustBeMoreThanZero();
