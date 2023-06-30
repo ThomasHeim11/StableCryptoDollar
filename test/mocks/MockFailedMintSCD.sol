d866fd53f712acb1a778d1ab589f2c5f4f1963b0
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-30 08:38:32 +0200

    update Makefile✅

--- test/mocks/MockFailedMintSCD.sol
+++ test/mocks/MockFailedMintSCD.sol
@@ -4,30 +4,30 @@ pragma solidity 0.8.19;
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockFailedMintDSC is ERC20Burnable, Ownable {
-    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
-    error DecentralizedStableCoin__BurnAmountExceedsBalance();
-    error DecentralizedStableCoin__NotZeroAddress();
+contract MockFailedMintSCD is ERC20Burnable, Ownable {
+    error StableCryptoDollar_AmountMustBeMoreThanZero();
+    error StableCryptoDollar__BurnAmountExceedsBalance();
+    error StableCryptoDollar__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+    constructor() ERC20("StableCryptoDollar", "SCD") {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+            revert StableCryptoDollar_AmountMustBeMoreThanZero();
         }
         if (balance < _amount) {
-            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+            revert StableCryptoDollar__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
 
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) {
-            revert DecentralizedStableCoin__NotZeroAddress();
+            revert StableCryptoDollar__NotZeroAddress();
         }
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+            revert StableCryptoDollar_AmountMustBeMoreThanZero();
         }
         _mint(_to, _amount);
         return false;
