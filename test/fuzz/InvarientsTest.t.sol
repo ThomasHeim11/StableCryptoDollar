6d443779aea5c79b879c9f0ebf4d9a1eef536fcd
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-01 16:07:23 +0200

    update unit test✅

--- test/fuzz/InvarientsTest.t.sol
+++ test/fuzz/InvarientsTest.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeeploySCD} from "../../script/DeploySCD.s.sol";
+import {SCDEngine} from "../../script/SCDEngine.s.sol";
 
 contract InvariantsTest is StdInvariant, Test {
     function setUp() external {}
