a6c6a42e8079e611e2fa896dc03d103b14ab23bd
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-02 19:28:34 +0200

    update unit test✅

--- test/fuzz/InvarientsTest.t.sol
+++ test/fuzz/InvarientsTest.t.sol
@@ -4,13 +4,19 @@ pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DeeploySCD} from "../../script/DeploySCD.s.sol";
+import {DeploySCD} from "../../script/DeploySCD.s.sol";
 import {SCDEngine} from "../../src/SCDEngine.sol";
+import {StableCryptoDollar} from "../../src/StableCryptoDollar.sol";
 
 contract InvariantsTest is StdInvariant, Test {
+    DeploySCD deployer;
+    SCDEngine scde;
+    StableCryptoDollar scd;
+
     function setUp() external {
-        DeploySCD deployer;
-        SCDEngine scde;
+        deployer = new DeploySCD();
+        (scde = deployer.run();
 
     }
+
 }
