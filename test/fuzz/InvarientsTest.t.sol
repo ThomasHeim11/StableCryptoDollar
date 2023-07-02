c5b9a168e44b004640925f47bea306ecc4f08bb0
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-02 19:29:35 +0200

    update unit test✅

--- test/fuzz/InvarientsTest.t.sol
+++ test/fuzz/InvarientsTest.t.sol
@@ -7,11 +7,13 @@ import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {DeploySCD} from "../../script/DeploySCD.s.sol";
 import {SCDEngine} from "../../src/SCDEngine.sol";
 import {StableCryptoDollar} from "../../src/StableCryptoDollar.sol";
+import {HelperConfig} from "../../src/HelperConfig.sol";
 
 contract InvariantsTest is StdInvariant, Test {
     DeploySCD deployer;
     SCDEngine scde;
     StableCryptoDollar scd;
+    HelperConfig config; 
 
     function setUp() external {
         deployer = new DeploySCD();
