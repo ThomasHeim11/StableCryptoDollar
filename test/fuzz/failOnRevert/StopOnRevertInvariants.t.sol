961332d68f52f0804acbd092d7cd9041b8bbf22d
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-03 14:26:08 +0200

    update fuzz test✅

---
+++ test/fuzz/failOnRevert/StopOnRevertInvariants.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeploySCD} from "../../../script/DeploySCD.s.sol";
+import {SCDEngine} from "../../../src/SCDEngine.sol";
+import {StableCryptoDollar} from "../../../src/StableCryptoDollar.sol";
+import {HelperConfig} from "../../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from ".././Handler.t.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeploySCD deployer;
+    SCDEngine scde;
+    StableCryptoDollar scd;
+    HelperConfig config; 
+    address weth;
+    address wbtc;
+    Handler handler;
+
+
+    function setUp() external {
+        deployer = new DeploySCD();
+        (scd, scde, config) = deployer.run();
+        (,, weth, wbtc, ) = config.activeNetworkConfig();
+        handler = new Handler(scde, scd);
+        targetContract(address(handler));
+
+    }
+
+    function invarient_protocolMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = scd.totalSupply();
+        uint256 totalWethDepositet = IERC20(weth).balanceOf(address(scde));
+        uint256 totalWbtcDepositet = IERC20(wbtc).balanceOf(address(scde));
+
+        uint256 wethValue = scde.getUsdValue(weth, totalWethDepositet);
+        uint256 wbtcValue = scde.getUsdValue(wbtc, totalWbtcDepositet);
+
+        console.log("weth value", wethValue);
+        console.log("wbtc value", wbtcValue);
+        console.log("total supply", totalSupply);
+        console.log("Times mint called:", handler.timeMintCalled());
+
+        assert (wethValue + wbtcValue > totalSupply);
+    }
+
+    function invariant_getterShouldNotRevert() public view {
+        scde.getLiquidationBonus();
+        scde.getPrecision();
+    }
+
+}
