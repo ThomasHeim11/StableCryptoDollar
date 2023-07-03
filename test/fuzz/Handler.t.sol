5e93a7c7a7f0144f3eaa8bc790a4146cc5231f29
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-03 10:57:58 +0200

    update fuzz test✅

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -2,5 +2,15 @@
 pragma solidity 0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
+import {SCDEngine} from "../../src/SCDEngine.sol";
+import {StableCryptoDollar} from "../../src/StableCryptoDollar.sol";
 
-contract Handler is Test {}
+contract Handler is Test {
+    SCDEngine scde;
+    StableCryptoDollar scd;
+
+    constructor(SCDEngine _scdEngine, StableCryptoDollar _scd) {
+        scde = _scdEngine;
+        scd = _scd;
+    }
+}
