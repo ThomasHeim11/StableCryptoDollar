f7c49e3610bb959031dca5b325c1dbdf62fe7355
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-09 09:55:53 +0200

    update deplpoy script✅

--- script/DeploySCD.s.sol
+++ script/DeploySCD.s.sol
@@ -1,32 +1,32 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {StableCryptoDollar} from "../src/StableCryptoDollar.sol";
 import {SCDEngine} from "../src/SCDEngine.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeploySCD is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
     function run() external returns (StableCryptoDollar, SCDEngine, HelperConfig) {
-        HelperConfig config = new HelperConfig();
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
 
-       (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc,
-       uint256 deployerKey) =
-        config.activeNetworkConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
-       tokenAddresses = [weth, wbtc];
-       priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
-
-       vm.startBroadcast(deployerKey);
-       StableCryptoDollar scd = new StableCryptoDollar();
-       SCDEngine engine = new SCDEngine(tokenAddresses, priceFeedAddresses, address(scd));
-
-       scd.transferOwnership(address(engine));
-       vm.stopBroadcast();
-       return (scd, engine, config);
+        vm.startBroadcast(deployerKey);
+        StableCryptoDollar scd = new StableCryptoDollar();
+        SCDEngine scdEngine = new SCDEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(scd)
+        );
+        scd.transferOwnership(address(scdEngine));
+        vm.stopBroadcast();
+        return (scd, scdEngine, helperConfig);
     }
 }
